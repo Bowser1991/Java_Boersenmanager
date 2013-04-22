a3263a177427f720986fa0feb126576619789057
daniel schlosser <daniel.schlosser@gmx.net>
2013-04-22 19:03:19 +0200

    Enum with constructor
    Habe hier mal das enum definiert. Wie du siehst steht im Konstruktor
    Class <?>... was einfach nur bedeutet, dass eine variable Anzahl von
    Parametern des Typs Class<?> übergeben werden können, ob er das so
    gemeint hatt stell ich jetzt an der Stelle mal dahin was anderes fällt
    mir allerdings nicht ein !!!

--- src/enums/StockGameCommandType.java
+++ src/enums/StockGameCommandType.java
@@ -1,14 +1,19 @@
 package enums;
 
 public enum StockGameCommandType {
-	HELP("help","* help, exit, crp, bus, ses, acw",""), 
-	EXIT("exit","* exit Programm",""), 
-	CREATEPLAYER("help","<name> * adds a new Player by name", "String.class"), 
-	BUYSHARE("help","* help, exit, crp, bus, ses, acw"), 
-	SELLSHARE("help","* help, exit, crp, bus, ses, acw"), 
-	ACCOUNTWORTH("help","* help, exit, crp, bus, ses, acw");
+	HELP("help","* help, exit, crp, bus, ses, acw"), 
+	EXIT("exit","* exit Programm"), 
+	CREATEPLAYER("crp","<name> * adds a new Player by name", String.class), 
+	BUYSHARE("bus","<Playername><Sharename><amount>* buys a Share",String.class, String.class, long.class), 
+	SELLSHARE("ses","<Playername><Sharename><amount>* sells a Share",String.class, String.class, long.class), 
+	ACCOUNTWORTH("acw","<name>* gets the account worth by playername",String.class);
 	
-	private StockGameCommandType(String command, String helptext, String classes ){
-		
+	private String commandname;
+	private String helptext;
+	private String commandclasses;
+	
+	private StockGameCommandType(String command, String helptext, Class<?>... T ){
+		commandname = command;
+		this.helptext = helptext;
 	}
 }
