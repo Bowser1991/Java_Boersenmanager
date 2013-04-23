56a9dfeb16cabaac31bbfb18bd6e934edd853b6d
manu888 <manuel-sky@web.de>
2013-04-23 15:58:12 +0200

    update wie im Klassendiagramm sichtbar.

---
+++ Command/StockGameCommandProcessor.java
@@ -0,0 +1,19 @@
+package Command;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import programmieren_boersenspiel2.AccountManager;
+
+
+public class StockGameCommandProcessor {
+	private BufferedReader shellReader = new BufferedReader(new InputStreamReader(System.in));
+	private PrintWriter shellWriter = new PrintWriter(System.out);
+	private AccountManager manager;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
